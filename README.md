9f8bb4222ff60bc5297718d91cdf7f6c23776407
Michal Ulewicz <michal.ulewicz@gmail.com>
2021-11-15 23:25:27 +0100

    fix

--- README.md
+++ README.md
@@ -34,5 +34,13 @@ source envs/ud20/bin/activate
 
 ## Executing preprocessing (preprocess.py) script - example for EN-PL dataset
 ```
-python.exe preprocess.py '--europarl_src=./data/en-pl/input/europarl/europarl-v7.pl-en.en' '--europarl_tgt=./data/en-pl/input/europarl/europarl-v7.pl-en.pl' '--tatoeba=./data/en-pl/input/tatoeba/Sentence pairs in English-Polish - 2021-11-14.tsv' '--output_src=./data/en-pl/preprocess/sentences.en' '--output_tgt=./data/en-pl/preprocess/sentences.pl' '--output_log=./data/en-pl/preprocess/log' '--min_tokens=2' '--max_tokens=50'
+python.exe preprocess.py '--europarl_src=./data/source/en-pl/europarl/europarl-v7.pl-en.en' '--europarl_tgt=./data/source/en-pl/europarl/europarl-v7.pl-en.pl' '--tatoeba=./data/source/en-pl/tatoeba/Sentence pairs in English-Polish - 2021-11-14.tsv' '--output_src=./data/en-pl/bitext_raw/en-pl.en.txt' '--output_tgt=./data/en-pl/bitext_raw/en-pl.pl.txt' '--output_log=./data/en-pl/bitext_raw/log' '--min_tokens=5' '--max_tokens=80' '--max_sentences=200000' '--split_ratio=0.8'
+```
+## Executing preprocessing (preprocess.py) script - example for EN-FR dataset - full
+```
+python.exe preprocess.py '--europarl_src=./data/source/en-fr/europarl/europarl-v7.fr-en.en' '--europarl_tgt=./data/source/en-fr/europarl/europarl-v7.fr-en.fr' '--tatoeba=./data/source/en-fr/tatoeba/Sentence pairs in English-French - 2021-11-15.tsv' '--output_src=./data/en-fr/bitext_raw/en-fr.en.txt' '--output_tgt=./data/en-fr/bitext_raw/en-fr.fr.txt' '--output_log=./data/en-fr/bitext_raw/log' '--min_tokens=5' '--max_tokens=80' '--max_sentences=0' '--split_ratio=0'
+```
+## Executing preprocessing (preprocess.py) script - example for EN-FR dataset - 200k
+```
+python.exe preprocess.py '--europarl_src=./data/source/en-fr/europarl/europarl-v7.fr-en.en' '--europarl_tgt=./data/source/en-fr/europarl/europarl-v7.fr-en.fr' '--tatoeba=./data/source/en-fr/tatoeba/Sentence pairs in English-French - 2021-11-15.tsv' '--output_src=./data/en-fr-200k/bitext_raw/en-fr.en.txt' '--output_tgt=./data/en-fr-200k/bitext_raw/en-fr.fr.txt' '--output_log=./data/en-fr-200k/bitext_raw/log' '--min_tokens=5' '--max_tokens=80' '--max_sentences=200000' '--split_ratio=0.2'
 ```
--- preprocess.py
+++ preprocess.py
@@ -22,7 +22,7 @@ def validate(text, context):
   if not validate_tokens(text, context["min_tokens"], context["max_tokens"]):
     return False, "Incorrect tokens length"
   if text in context["map"]:
-    return False, "Duplicate"
+    return False, "Duplicate" #we do not allow for duplicates both in source and target language
   else:
     context["map"][text] = {}
   return True, ""
@@ -36,8 +36,8 @@ def process_europarl(src, tgt, context):
     so, sm = validate(s, context)
     to, tm = validate(t, context)
     if so and to:
-      context['src'].append(s)
-      context['tgt'].append(t)
+      context["europarl"]['src'].append(s)
+      context["europarl"]['tgt'].append(t)
     else:
       context['log'].append(f'Skipping EUROPARL sentence {counter} / SRC: {s} / TGT: {t} / SRC MSG: {sm} / TGT MSG: {tm}')
 
@@ -52,8 +52,8 @@ def process_tatoeba(src, context):
       so, sm = validate(s, context)
       to, tm = validate(t, context)
       if so and to:
-        context['src'].append(s)
-        context['tgt'].append(t)
+        context["tatoeba"]['src'].append(s)
+        context["tatoeba"]['tgt'].append(t)
       else:
         context['log'].append(f'Skipping TATOEBA sentence {counter} / SRC: {s} / TGT: {t} / SRC MSG: {sm} / TGT MSG: {tm}')
 
@@ -77,14 +77,24 @@ if __name__ == '__main__':
                       help='Minimal number of tokens')
   parser.add_argument('--max_tokens', type=int, default=100,
                       help='Maximal number of tokens')
+  parser.add_argument('--max_sentences', type=int, default=0,
+                      help='Maximal number of sentences')
+  parser.add_argument('--split_ratio', type=float, default=0,
+                      help='Split ratio between tatoeba and europarl, applies only if max_sentences is different than 0')
 
   args = parser.parse_args()
 
   t0 = time.time()
 
   context = {
-    "src": [],
-    "tgt": [],
+    "europarl": {
+      "src": [],
+      "tgt": []
+    },
+    "tatoeba": {
+      "src": [],
+      "tgt": []
+    },
     "log": [],
     "map": {},
     "min_tokens": args.min_tokens,
@@ -101,11 +111,34 @@ if __name__ == '__main__':
   process_europarl(europarl_src, europarl_tgt, context)
   process_tatoeba(tatoeba, context)
 
+  europarl_total = len(context["europarl"]["src"])
+  tatoeba_total = len(context["tatoeba"]["src"])
+
+  if args.max_sentences > 0:
+    tatoeba_count = round(args.max_sentences * args.split_ratio)
+    europarl_count = args.max_sentences - tatoeba_count
+  else:
+    europarl_count = europarl_total
+    tatoeba_count = tatoeba_total
+
+  if tatoeba_count > tatoeba_total:
+    tatoeba_count = tatoeba_total
+  
+  if europarl_count > europarl_total:
+    europarl_count = europarl_total
+
+  print(f'Saving {tatoeba_count} tatoeba sentences and {europarl_count} europarl sentences.')
+
   with open(args.output_src, 'w', encoding='utf8') as f:
-    f.write('\n'.join(context['src']))
+    f.write('\n'.join(context['europarl']['src'][0:europarl_count]))
+    f.write("\n")
+    f.write('\n'.join(context['tatoeba']['src'][0:tatoeba_count]))
 
   with open(args.output_tgt, 'w', encoding='utf8') as f:
-    f.write('\n'.join(context['tgt']))
+    f.write('\n'.join(context['europarl']['tgt'][0:europarl_count]))
+    f.write("\n")
+    f.write('\n'.join(context['tatoeba']['tgt'][0:tatoeba_count]))
+
   with open(args.output_log, 'w', encoding='utf8') as f:
     f.write('\n'.join(context['log']))
   
