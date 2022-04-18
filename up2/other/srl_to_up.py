0ff66fa92fa07b67e338c7dfc0f90069e986956c
Michał Ulewicz <Michal.Ulewicz@kyndryl.com>
2022-04-18 10:41:14 +0200

    srl_to_up script

---
+++ up2/other/srl_to_up.py
@@ -0,0 +1,111 @@
+'''
+Creates CoNLLu-p file with SRL annotations only
+'''
+import logging
+import time
+import argparse
+from conllup import parse
+import json
+from conllup.model.tree import Tree
+from conllup.model.column import Column, ColumnType
+from conllup.model.token import Token
+from os import path
+import glob
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)s %(message)s',
+    datefmt='%Y/%m/%d %H:%M:%S',
+    level=logging.INFO,
+    handlers=[
+        logging.FileHandler("./logs/srl_to_up.log"),
+        logging.StreamHandler()
+    ]
+)
+
+
+def process(input: Tree) -> Tree:
+    
+    output_columns = [
+        Column("ID", ColumnType.ID),
+        Column("FORM"),
+        Column("LEMMA"),
+        Column("UPOS"),
+        Column("XPOS"),
+        Column("FEATS"),
+        Column("HEAD"),
+        Column("DEPREL"),
+        Column("UP:PRED", ColumnType.UP_PRED),
+        Column("UP:ARGHEADS", ColumnType.UP_ARGHEADS),
+    ]
+
+    output = Tree(output_columns)
+    for m in input.get_metadata():
+        v = input.get_metadata(m).get_value()
+        output.add_metadata(m, v)
+
+    import copy
+    for f in input.get_frames():
+        output.add_frame(copy.deepcopy(f))
+    
+    for t in input.get_tokens():
+        token = output.add_token(t.get_id())
+        for c in output.get_columns_by_type(ColumnType.BASIC):
+            token.set_attribute(c, t.get_attribute(c))
+
+    return output
+
+def process_file(input, output):
+    logging.info(f'Starting srl_to_up')
+
+    t0 = time.time()
+
+    input_columns = [
+        Column("ID", ColumnType.ID),
+        Column("FORM"),
+        Column("LEMMA"),
+        Column("UPOS"),
+        Column("XPOS"),
+        Column("FEATS"),
+        Column("HEAD"),
+        Column("DEPREL"),
+        Column("UP:FLAG", ColumnType.UP_FLAG),
+        Column("UP:PRED", ColumnType.UP_PRED),
+        Column("UP:FRAME", ColumnType.UP_FRAME),
+    ]
+
+    try:
+        counter = 0
+        fi = open(input, encoding='utf8')
+        fo = open(output, "w", encoding='utf8')
+        for tr in parse(fi, input_columns):
+            counter += 1
+            print(f"sentence {counter}")
+            new_tree = process(tr)
+            str_tree = new_tree.to_conllup(counter == 1)
+            fo.write(str_tree + "\n\n")
+
+    except Exception as e:
+        logging.error(e)
+        raise e
+
+    t1 = time.time()
+
+    logging.info(
+        f'Total srl_to_up time: {(t1 - t0):.2f} s, processed sentences: {counter}')
+
+def srl_to_up(input, output):
+    process_file(input, output)
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        description='srl_to_up')
+    parser.add_argument('--input', type=str,
+                        help='Input file')
+    parser.add_argument('--output', type=str,
+                        help='Output conllu file')
+
+    args = parser.parse_args()
+
+    srl_to_up(args.input, args.output)
+    
